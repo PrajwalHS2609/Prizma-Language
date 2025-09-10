a13dc2c4b15bdc25fb45bd66ef8b74c35111b105
Prajwal H S <prajwalhs4india@gmail.com>
2025-09-10 19:17:20 +0530

    changes favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="./favicon.png" />
       </head>
       <body>
         <NavBar />
