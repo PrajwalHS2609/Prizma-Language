c647388d985ce5abc3c2d6595e7c9b5e127474fa
Prajwal H S <prajwalhs4india@gmail.com>
2025-09-23 12:33:26 +0530

    review changes

--- src/components/GermanCourse/GermanA1/GermanA1Reviews.jsx
+++ src/components/GermanCourse/GermanA1/GermanA1Reviews.jsx
@@ -1,29 +1,15 @@
-"use client"
-import React, { useEffect } from "react";
-import "./GermanA1.css"
+"use client";
+import React from "react";
+import "./GermanA1.css";
 const GermanA1Reviews = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apps.elfsight.com/p/platform.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   return (
     <div className="reviewsContainer">
-      <script
-        src="https://static.elfsight.com/platform/platform.js"
-        data-use-service-core
-        defer
-      ></script>
-      <div
-        class="elfsight-app-c47b3300-f2fb-484e-9421-bb71c2e020f1"
-        id="reviews"
-        data-elfsight-app-lazy
-      ></div>
+      <iframe
+        src="https://widgets.sociablekit.com/google-reviews/iframe/25601963"
+        frameborder="0"
+        width="100%"
+        height="1000"
+      ></iframe>
     </div>
   );
 };
