3e6f89c128190d831454c36a95a8bd5c52e7236d
Prajwal H S <prajwalhs4india@gmail.com>
2025-10-11 02:42:48 +0530

    resp changes

--- src/components/HomePage/HomeFeatures/HomeFeaturesMain.jsx
+++ src/components/HomePage/HomeFeatures/HomeFeaturesMain.jsx
@@ -30,7 +30,7 @@ const HomeFeaturesMain = () => {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
