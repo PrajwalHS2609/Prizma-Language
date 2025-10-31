e935c78d5180b8c300b190ed8097043d0ece8e7c
Prajwal H S <prajwalhs4india@gmail.com>
2025-10-31 16:57:17 +0530

    dynamic slug changes3

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -12,7 +12,7 @@ import BlogSidebar from "./../../components/BlogPage/BlogSidebar/BlogSidebar";
 import "@/components/Styles.css";
 import ContentHeader from "./../../components/ContentHeader/ContentHeader";
 
-export const revalidate = 60;
+export const revalidate = 0;
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{
   _id,
